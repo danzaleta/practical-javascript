1705d35663b2a8c5f7d19ed0d5e77215dcc426de
Daniel Zaleta <zaletadaniel@gmail.com>
2022-03-08 16:00:21 -0600

    calc of shapes for cosole with default values

--- shapes/shapes.js
+++ shapes/shapes.js
@@ -2,13 +2,12 @@
 console.group(`Square`);
 
 const squareSide = 5;
-console.log(`Square side value: ${squareSide}cm`);
 
-const squarePerimeter = squareSide * 4;
-console.log(`Square perimeter value: ${squarePerimeter}cm`);
+const calcSquarePerimeter = side => side*4;
+const calcSquareArea = side => side**2;
 
-const squareArea = squareSide ** 2;
-console.log(`Square area value: ${squareArea}cm^2`);
+console.log(`Square perimeter value: ${calcSquarePerimeter(squareSide)}cm`);
+console.log(`Square area value: ${calcSquareArea(squareSide)}cm^2`);
 
 console.groupEnd();
 
@@ -20,16 +19,13 @@ console.group(`Triangle`);
 const triangleSideA = 6;
 const triangleSideB = 6;
 const triangleSideC = 4;
-console.log(`Triangle side values: A:${triangleSideA}cm, B:${triangleSideB}cm, C:${triangleSideC}cm`);
-
 const triangleHeight = 5.5;
-console.log(`Triangle height value: ${triangleHeight}cm`)
 
-const trianglePerimeter = triangleSideA + triangleSideB + triangleSideC ;
-console.log(`Triangle perimeter value: ${trianglePerimeter}cm`);
+const calcTrianglePerimeter = (sideA, sideB, base)=> sideA+sideB+base;
+const calcTriangleArea = (base, height)=> (base*height)/2;
 
-const triangleArea = (triangleSideC*triangleHeight) / 2;
-console.log(`Square area value: ${triangleArea}cm^2`);
+console.log(`Triangle perimeter value: ${calcTrianglePerimeter(triangleSideA, triangleSideB, triangleSideC)}cm`);
+console.log(`Square area value: ${calcTriangleArea(triangleSideC, triangleHeight)}cm^2`);
 
 console.groupEnd();
 
@@ -40,6 +36,12 @@ console.group(`Circle`);
 
 const circleRadius = 4;
 
-const circleDiameter = circleRadius * PictureInPictureWindow;
+const calcCircleDiameter = radius => radius*2;
+const calcCirclePerimeter = radius => calcCircleDiameter(radius)*Math.PI;
+const calcCircleArea = radius => (radius**2)*Math.PI;
+
+console.log(`Circle diameter: ${calcCircleDiameter(circleRadius)}cm`);
+console.log(`Circle perimeter: ${calcCirclePerimeter(circleRadius)}cm`);
+console.log(`Circle area: ${calcCircleArea(circleRadius)}cm^2`);
 
 console.groupEnd();
