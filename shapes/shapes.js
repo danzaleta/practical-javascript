5d5a23977e512bb06e168a646a57b597ec4663d3
Daniel Zaleta <zaletadaniel@gmail.com>
2022-03-08 18:18:55 -0600

    shape calculator designed

--- shapes/shapes.js
+++ shapes/shapes.js
@@ -1,47 +1,76 @@
 // Square
-console.group(`Square`);
-
-const squareSide = 5;
-
 const calcSquarePerimeter = side => side*4;
 const calcSquareArea = side => side**2;
 
-console.log(`Square perimeter value: ${calcSquarePerimeter(squareSide)}cm`);
-console.log(`Square area value: ${calcSquareArea(squareSide)}cm^2`);
-
-console.groupEnd();
-
-
-
 // Triangle
-console.group(`Triangle`);
-
-const triangleSideA = 6;
-const triangleSideB = 6;
-const triangleSideC = 4;
-const triangleHeight = 5.5;
-
 const calcTrianglePerimeter = (sideA, sideB, base)=> sideA+sideB+base;
 const calcTriangleArea = (base, height)=> (base*height)/2;
 
-console.log(`Triangle perimeter value: ${calcTrianglePerimeter(triangleSideA, triangleSideB, triangleSideC)}cm`);
-console.log(`Square area value: ${calcTriangleArea(triangleSideC, triangleHeight)}cm^2`);
-
-console.groupEnd();
-
-
-
 // Circle
-console.group(`Circle`);
-
-const circleRadius = 4;
-
 const calcCircleDiameter = radius => radius*2;
 const calcCirclePerimeter = radius => calcCircleDiameter(radius)*Math.PI;
 const calcCircleArea = radius => (radius**2)*Math.PI;
 
-console.log(`Circle diameter: ${calcCircleDiameter(circleRadius)}cm`);
-console.log(`Circle perimeter: ${calcCirclePerimeter(circleRadius)}cm`);
-console.log(`Circle area: ${calcCircleArea(circleRadius)}cm^2`);
 
-console.groupEnd();
+
+// HTML interaction
+function SquarePerimeter() {
+    const input = document.getElementById("square-input");
+    const value = parseFloat(input.value);
+
+    const perimeter = calcSquarePerimeter(value);
+    alert(perimeter);
+}
+function SquareArea() {
+    const input = document.getElementById("square-input");
+    const value = parseFloat(input.value);
+
+    const area = calcSquareArea(value);
+    alert(area);
+}
+
+function TrianglePerimeter() {
+    const inputA = document.getElementById("triangle-sideA-input");
+    const valueA = parseFloat(inputA.value);
+
+    const inputB = document.getElementById("triangle-sideB-input");
+    const valueB = parseFloat(inputB.value);
+
+    const inputBase = document.getElementById("triangle-base-input");
+    const base = parseFloat(inputBase.value);
+
+    const perimeter = calcTrianglePerimeter(valueA, valueB, base);
+    alert(perimeter);
+}
+function TriangleArea() {
+    const inputBase = document.getElementById("triangle-base-input");
+    const base = parseFloat(inputBase.value);
+
+    const inputHeight = document.getElementById("triangle-height-input");
+    const height = parseFloat(inputHeight.value);
+
+    const area = calcTriangleArea(base, height);
+    alert(area);
+}
+
+function CircleDiameter() {
+    const input = document.getElementById("circle-input");
+    value = parseFloat(input.value);
+
+    const diameter = calcCircleDiameter(value);
+    alert(diameter);
+}
+function CirclePerimeter() {
+    const input = document.getElementById("circle-input");
+    value = parseFloat(input.value);
+
+    const perimeter = calcCirclePerimeter(value);
+    alert(perimeter);
+}
+function CircleArea() {
+    const input = document.getElementById("circle-input");
+    value = parseFloat(input.value);
+
+    const area = calcCircleArea(value);
+    alert(area);
+}
